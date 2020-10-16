30e8c814230ab705d8da48e1700e355355e41008
Felipe Souza <felipedb91@gmail.com>
2020-10-15 23:27:34 -0300

    feat: insert on project pages CreateOrphanage and OrphanageDetails

--- web/src/pages/OrphanagesMaps/index.tsx
+++ web/src/pages/OrphanagesMaps/index.tsx
@@ -3,9 +3,17 @@ import React from 'react';
 import './styles.css';
 import mapMarker from '../../images/map-marker.svg';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import Leaflet from 'leaflet';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarker,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 const OrphanagesMaps: React.FC = () => {
   return (
@@ -27,8 +35,21 @@ const OrphanagesMaps: React.FC = () => {
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[-22.9070902, -43.2037672]} icon={mapIcon}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Lar das meninas
+            <Link to={`/orphanage/1`}>
+              <FiArrowRight size={20} color="#FFF" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanage/create" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
     </div>
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import CreateOrphanage from './pages/CreateOrphanage';
 import Landing from './pages/Landing/inde';
+import Orphanage from './pages/Orphanage';
 import OrphanagesMaps from './pages/OrphanagesMaps';
 
 const AppRoutes: React.FC = () => {
@@ -9,6 +11,8 @@ const AppRoutes: React.FC = () => {
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/maps" component={OrphanagesMaps} />
+        <Route path="/orphanage/create" component={CreateOrphanage} />
+        <Route path="/orphanage/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
